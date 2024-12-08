fb501439b902688578532ed6de9f7cd3c41b8f86
Aditya <lapratomo24@yahoo.com>
2024-12-08 19:41:04 +0100

    added write functionality to add brightness

--- fuji.py
+++ fuji.py
@@ -4,11 +4,22 @@ from PIL import Image
 
 
 def main():
-    with open("fuji.csv", encoding='utf-8') as file:
-        reader = csv.DictReader(file)
+    with open("fuji.csv", encoding='utf-8') as views, open("analysis.csv", "w", encoding='utf-8') as analysis:
+        reader = csv.DictReader(views)
+        writer = csv.DictWriter(analysis, fieldnames=reader.fieldnames + ["brightness"])
+        writer.writeheader()
+        
         for row in reader:
             brightness = calc_brightness(f"{row['id']}.jpg")
-            print(round(brightness, 2))
+            # print(round(brightness, 2))
+            writer.writerow(
+                {
+                    "id": row["id"],
+                    "english_title": row["english_title"],
+                    "japanese_title": row["japanese_title"],
+                    "brightness": round(brightness, 2)
+                }
+            )
             
 
 def calc_brightness(filename):
