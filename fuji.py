594d04047c011b01225d084936aebb67aa8fa0d3
Aditya <lapratomo24@yahoo.com>
2024-12-08 21:58:12 +0100

    refactored write functionality

--- fuji.py
+++ fuji.py
@@ -4,22 +4,23 @@ from PIL import Image
 
 
 def main():
-    with open("fuji.csv", encoding='utf-8') as views, open("analysis.csv", "w", encoding='utf-8') as analysis:
+    with open("fuji.csv", encoding='utf-8') as views, open("analysis.csv", "w", newline='', encoding='utf-8') as analysis:
         reader = csv.DictReader(views)
         writer = csv.DictWriter(analysis, fieldnames=reader.fieldnames + ["brightness"])
         writer.writeheader()
         
         for row in reader:
-            brightness = calc_brightness(f"{row['id']}.jpg")
+            row["brightness"] = round(calc_brightness(f"{row['id']}.jpg"), 2)
+            writer.writerow(row)
             # print(round(brightness, 2))
-            writer.writerow(
-                {
-                    "id": row["id"],
-                    "english_title": row["english_title"],
-                    "japanese_title": row["japanese_title"],
-                    "brightness": round(brightness, 2)
-                }
-            )
+            # writer.writerow(
+            #     {
+            #         "id": row["id"],
+            #         "english_title": row["english_title"],
+            #         "japanese_title": row["japanese_title"],
+            #         "brightness": round(brightness, 2)
+            #     }
+            # )
             
 
 def calc_brightness(filename):
